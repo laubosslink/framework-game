631d53421ebba289719b026b108a309c1096fd15
laubosslink <laubosslink@society-lbl.com>
2014-12-15 11:24:08 +0100

    Example - Sortie : ajout d'un main et separation des classes, pour pouvoir tester le fonctionnement #4

--- examples/SortieExample/ExampleTextuel.java
+++ examples/SortieExample/ExampleTextuel.java
@@ -1,3 +1,5 @@
+package SortieExample;
+
 /**
  * ENSICAEN 6 Boulevard Marechal Juin F-14050 Caen Cedex
  * 
@@ -10,61 +12,9 @@
  * <laurent.parmentier@ecole.ensicaen.fr, laubosslink@society-lbl.com>
  */
 
-import java.util.Observable;
-
-import Sortie.*;
-
-public class Joueur extends Observable implements ObservableTexte {
-    private String nomJoueur;
-    private boolean premiereInstance=true;
-
-    public void init(String nom){
-        setNomJoueur(nom);
-    }
-
-    public void setNomJoueur(String nom){
-        this.nomJoueur = nom;
-        setChanged();
-        notifyObservers();
-    }
-
-    public String toString(){
-        if(premiereInstance)
-        {
-            /**
-             * remarque : normalement c'est le constructeur Joueur qui devrait mettre premiereInstance a false, 
-             * mais il y a un risque qu'il soit change avant l'appel de cette fonction
-             * a l'avenirs resoudre ce probleme par le passage d'un Object en param 
-             */
-            this.premiereInstance = false;
-            return "Creation du joueur : " + this.nomJoueur;
-        }
-
-        return "Changement du nom du joueur : " + this.nomJoueur;
-    }
-}
-
-public class Partie {
-    private Joueur j1, j2;
-
-    private AffichageTexte at1, at2;
-
-    public Partie()
+class ExampleTextuel {
+    public static void main(String[] args)
     {
-        System.out.println("Creation d'une partie.");
-        System.out.println("Creation des joueurs.");
-
-        at1 = new AffichageTexte("Objet J1", j1);
-        at2 = new AffichageTexte("Objet J2", j2);
-
-        j1.init("JB");
-        j2.init("Quentin");
-
-        j1.setNomJoueur("Laurent");
+        new Partie();
     }
-}
-
-public static void main(String[] args)
-{
-    new Partie();
 }
