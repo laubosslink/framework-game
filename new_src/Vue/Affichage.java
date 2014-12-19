7c4d74b4574cbbf9a0e4af802324dcc892969850
Parmentier Laurent <laubosslink@society-lbl.com>
2014-12-19 15:02:31 +0100

    Génération du code de l'uml dans new_src et correction de bug sur l'uml

---
+++ new_src/Vue/Affichage.java
@@ -0,0 +1,68 @@
+package Vue;
+
+import java.util.*;
+
+
+/**
+ * Class Affichage
+ */
+public class Affichage {
+
+  //
+  // Fields
+  //
+
+  private List<Vue::VueType> l;
+  
+  //
+  // Constructors
+  //
+  public Affichage () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of l
+   * @param newVar the new value of l
+   */
+  private void setL (List<Vue::VueType> newVar) {
+    l = newVar;
+  }
+
+  /**
+   * Get the value of l
+   * @return the value of l
+   */
+  private List<Vue::VueType> getL () {
+    return l;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @param        v
+   */
+  public void ajouterVue(Vue.VueType v)
+  {
+    this.l.add(v);
+  }
+
+
+  /**
+   */
+  public void Affichage()
+  {
+    this.l = new ArrayList<>();
+  }
+
+
+}
