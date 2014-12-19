a858adc5d52bd962950d396f991b0e3d7d728be9
Quentin <quentin.derory@ecole.ensicaen.fr>
2014-12-19 15:30:56 +0100

    Ajout des sources de l'uml dans le projet 8dragons

---
+++ examples/games/8dragons/8dragons/src/Controleur/Action.java
@@ -0,0 +1,38 @@
+package Controleur;
+
+
+/**
+ * Interface Action
+ */
+public interface Action {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void execute();
+
+
+  /**
+   * @return       boolean
+   */
+  public boolean estValide();
+
+
+}
---
+++ examples/games/8dragons/8dragons/src/Modele/Commande.java
@@ -0,0 +1,46 @@
+package Modele;
+
+
+/**
+ * Class Commande
+ */
+public class Commande {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public Commande () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void execute()
+  {
+  }
+
+
+  /**
+   */
+  public void undo()
+  {
+  }
+
+
+}
