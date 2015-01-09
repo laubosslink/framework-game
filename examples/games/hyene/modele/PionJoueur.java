49fef2c31bd10f54f8bd46fb1f17cee42db8560e
Mohammed Benseddik <benseddikmohammed@gmail.com>
2015-01-09 22:16:46 +0100

    Ajout de la classe PionJoeur pour differencier les joueurs de la Hyene, suppression de la confusion. Reste a remplire la classe.

---
+++ examples/games/hyene/modele/PionJoueur.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package games.hyene.modele;
+
+import games.hyene.modele.Pion;
+
+/**
+ *
+ * @author benseddik
+ */
+class PionJoueur extends Pion {
+
+    public PionJoueur() {
+    }
+    
+}
